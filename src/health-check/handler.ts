f90efd1f7fa14175f60391b19c2b83830e1dc7fd
Evgeny Mahnovets <mail@emahnovets.dev>
2022-08-06 14:21:26 +0200

    feat: return app version in healthcheck

--- src/health-check/handler.ts
+++ src/health-check/handler.ts
@@ -1,10 +1,12 @@
 import type { Handler } from 'aws-lambda';
+import packageJson from '../../package.json';
 
 export const healthCheck: Handler = async () => {
   return {
     statusCode: 200,
     body: JSON.stringify({
       status: 'ok',
+      version: packageJson.version,
     }),
   };
 };
