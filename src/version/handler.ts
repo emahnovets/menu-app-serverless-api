3c45b284dc8bacdd5627d54dfd8a64a04180b93b
Evgeny Mahnovets <mail@emahnovets.dev>
2022-08-07 15:29:27 +0200

    fix: package all functions separately
    Package all functions separately to reduce bundle size for each one

--- src/version/handler.ts
+++ src/version/handler.ts
@@ -1,11 +1,10 @@
 import type { Handler } from 'aws-lambda';
-import packageJson from '../../package.json';
 
 export const version: Handler = async () => {
   return {
     statusCode: 200,
     body: JSON.stringify({
-      version: packageJson.version,
+      version: process.env.APP_VERSION,
     }),
   };
 };
